d7b181fa91aed9cb3198a9df47beeb488444439a
Telo Gaspar <1231304@isep.ipp.pt>
2024-05-19 19:59:01 +0100

    Added viva properties. Algorithm now works better (not fully fixed). Added milestone2 executive summary.

--- src/test/scala/pj/domain/schedule/generators/ResourceGenerators.scala
+++ src/test/scala/pj/domain/schedule/generators/ResourceGenerators.scala
@@ -2,7 +2,7 @@ package pj.domain.schedule.generators
 
 import org.scalacheck.Gen
 import pj.domain.schedule.Domain.Resource
-import pj.domain.schedule.SimpleTypes.{resourceId, resourceName}
+import pj.domain.schedule.SimpleTypes.{agendaDuration, resourceId, resourceName}
 import pj.domain.schedule.generators.AvailabilityGenerators.generateAvailability
 
 import scala.util.Random
@@ -11,7 +11,7 @@ object ResourceGenerators:
 
   def generateThreeDigitNumber: Vector[String] =
     val random = new Random()
-    val numbers = Random.shuffle((0 to 999).map(n => f"$n%03d")).take(999).toVector // Generate a shuffled sequence of unique 3-digit numbers as strings
+    val numbers = Random.shuffle((1 to 999).map(n => f"$n%03d")).take(999).toVector // Generate a shuffled sequence of unique 3-digit numbers as strings
     numbers
 
 
@@ -59,7 +59,7 @@ object ResourceGenerators:
     } yield resourceId
 
 
-  def generateResources: Gen[Resource] =
+  def generateResources(duration: agendaDuration): Gen[Resource] =
     for {
       uniqueNumber <- Gen.oneOf(generateThreeDigitNumber)
       prefix <- Gen.oneOf(true, false)
@@ -67,7 +67,27 @@ object ResourceGenerators:
       namePrefix = if (prefix) "Teacher " else "External "
       id <- generateResourceId(idPrefix, uniqueNumber)
       name <- generateResourceName(namePrefix, uniqueNumber)
-      availabilities <- Gen.listOfN(5, generateAvailability)
+      availabilities <- Gen.listOfN(10, generateAvailability(duration))
     } yield Resource(id, name, availabilities)
 
 
+  def generateUniqueResource(generatedIds: Set[String], duration: agendaDuration): Gen[Resource] = {
+    for {
+      uniqueNumber <- Gen.oneOf(generateThreeDigitNumber)
+      prefix <- Gen.oneOf(true, false)
+      idPrefix = if (prefix) "T" else "E"
+      namePrefix = if (prefix) "Teacher " else "External "
+      id <- generateResourceId(idPrefix, uniqueNumber)
+      name <- generateResourceName(namePrefix, uniqueNumber)
+      availabilities <- Gen.listOfN(10, generateAvailability(duration))
+    } yield Resource(id, name, availabilities)
+  }.suchThat(resource => !generatedIds.contains(resource.id.to))
+  
+  def generateUniqueResources(n: Int, duration: agendaDuration, generatedIds: Set[String]): Gen[List[Resource]] =
+    if (n <= 0)
+      Gen.const(List.empty[Resource])
+    else
+      for {
+        resource <- generateUniqueResource(generatedIds, duration)
+        resources <- generateUniqueResources(n - 1, duration, generatedIds + resource.id.to)
+      } yield resource :: resources
