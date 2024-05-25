5f8a4a3cdbb5635fe84f94215f670eded86e0404
LuisGitBucket <1230196@isep.ipp.pt>
2024-05-25 01:00:19 +0100

    Milestone 2 fixes.

--- src/test/scala/pj/domain/schedule/generators/VivaGenerators.scala
+++ src/test/scala/pj/domain/schedule/generators/VivaGenerators.scala
@@ -5,7 +5,7 @@ import pj.domain.Result
 import pj.domain.schedule.Algorithm
 import pj.domain.schedule.Domain.{Availability, Resource, Viva}
 import pj.domain.schedule.SimpleTypes.{Role, agendaDuration, resourceId, vivaStudent, vivaTitle}
-import pj.domain.schedule.generators.ResourceGenerators.{generateAdvisor, generateCoadvisor, generatePresident, generateResources, generateSupervisor, generateThreeDigitNumber, generateUniqueResources}
+import pj.domain.schedule.generators.ResourceGenerators.{generateThreeDigitNumber, generateUniqueResources}
 import pj.domain.schedule.generators.AvailabilityGenerators.generateDuration
 
 import scala.collection.immutable.ListMap
@@ -32,23 +32,25 @@ object VivaGenerators:
       titleF <- vivaTitle.from(title).fold(_ => Gen.fail, title => Gen.const(title))
     } yield titleF
 
-  def generateRoles(resources: List[Resource]): Gen[ListMap[resourceId, Role]] =
+  def generateRoles(teachers: List[Resource], externals: List[Resource]): Gen[ListMap[resourceId, Role]] =
+    val allResources = teachers ++ externals
     for {
-      president <- Gen.oneOf(resources)
-      advisor <- Gen.oneOf(resources)
-      remainingRoles = 5 - 2 // Subtract the number of mandatory roles from the total
-      coadvisorCount <- Gen.chooseNum(0, Math.min(3, remainingRoles))
-      coadvisors <- Gen.pick(coadvisorCount, resources)
+      president <- Gen.oneOf(teachers)
+      advisor <- Gen.oneOf(teachers.filterNot(_ == president))
+      remainingRoles = 3
+      coadvisorCount <- Gen.chooseNum(0, Math.min(3, Math.min(remainingRoles, allResources.size - 2))) // Maximum of 3 coadvisors, considering president and advisor are already picked
+      coadvisors <- Gen.pick(coadvisorCount, allResources.filterNot(res => res == president || res == advisor))
       remainingRoles2 = remainingRoles - coadvisorCount
-      supervisorCount <- Gen.chooseNum(0, Math.min(3, remainingRoles2))
-      supervisors <- Gen.pick(supervisorCount, resources)
+      supervisorCount <- Gen.chooseNum(0, Math.min(3, Math.min(remainingRoles2, allResources.size - 2 - coadvisorCount))) // Maximum of 3 supervisors
+      supervisors <- Gen.pick(supervisorCount, allResources.filterNot(res => res == president || res == advisor || coadvisors.contains(res)))
     } yield
       val roles = List(Some(president.id -> Role.President), Some(advisor.id -> Role.Advisor)) ++
         coadvisors.map(coadvisor => Some(coadvisor.id -> Role.Coadvisor)) ++
         supervisors.map(supervisor => Some(supervisor.id -> Role.Supervisor))
       ListMap(roles.flatten *)
 
-  def generateViva(resources: List[Resource]): Gen[Viva] =
+
+  def generateViva(teachers: List[Resource], externals: List[Resource]): Gen[Viva] =
     val prefixStudent = Gen.const("Student ")
     val prefixTitle = Gen.const("Title ")
     val uniqueNumber = generateThreeDigitNumber
@@ -56,10 +58,9 @@ object VivaGenerators:
     for {
       ps <- prefixStudent
       pt <- prefixTitle
-
       student = vivaStudent.from(ps + Gen.oneOf(uniqueNumber).sample.getOrElse("0"))
       title = vivaTitle.from(pt + Gen.oneOf(uniqueNumber).sample.getOrElse("0"))
-      roles <- generateRoles(resources)
+      roles <- generateRoles(teachers, externals)
       studentF <- student.fold(_ => Gen.fail, student => Gen.const(student))
       titleFinal <- title.fold(_ => Gen.fail, title => Gen.const(title))
     } yield Viva(studentF, titleFinal, roles)
@@ -69,11 +70,13 @@ object VivaGenerators:
   def generateInstersectedAvailabilities: Gen[Result[(List[Availability], List[Resource], List[Resource], agendaDuration)]] =
     for {
       agendaDuration <- generateDuration
-      teachers <- generateUniqueResources(5, agendaDuration, Set.empty)
-      externals <- generateUniqueResources(5, agendaDuration, teachers.map(_.id.to).toSet)
-      allResources = teachers ++ externals
-      finalResources <- Gen.pick(5, allResources)
-      vivas <- generateViva(finalResources.toList)
+      teachers <- generateUniqueResources(5, agendaDuration, Set.empty, true)
+      externals <- generateUniqueResources(5, agendaDuration, teachers.map(_.id.to).toSet, false)
+      numTeachers <- Gen.chooseNum(2, 5)
+      numExternals <- Gen.chooseNum(0, 3)
+      teachersF <- Gen.pick(numTeachers, teachers)
+      externalsF <- Gen.pick(numExternals, externals)
+      vivas <- generateViva(teachersF.toList, externalsF.toList)
     } yield
       val scheduledVivasResult = Algorithm.intersectAvailabilities(vivas, teachers, externals, agendaDuration)
       scheduledVivasResult.map(scheduledVivas => (scheduledVivas, teachers, externals, agendaDuration))
@@ -81,11 +84,13 @@ object VivaGenerators:
   def generateScheduledVivas: Gen[Result[(List[(Viva, Availability)], List[Resource], List[Resource], Int)]] =
     for {
       agendaDuration <- generateDuration
-      teachers <- generateUniqueResources(5, agendaDuration, Set.empty)
-      externals <- generateUniqueResources(5, agendaDuration, teachers.map(_.id.to).toSet)
-      allResources = teachers ++ externals
-      finalResources <- Gen.pick(5, allResources)
-      vivas <- Gen.listOfN(5, generateViva(finalResources.toList))
+      teachers <- generateUniqueResources(5, agendaDuration, Set.empty, true)
+      externals <- generateUniqueResources(5, agendaDuration, teachers.map(_.id.to).toSet, false)
+      numTeachers <- Gen.chooseNum(2, 5)
+      numExternals <- Gen.chooseNum(0, 3)
+      teachersF <- Gen.pick(numTeachers, teachers)
+      externalsF <- Gen.pick(numExternals, externals)
+      vivas <- Gen.listOfN(5, generateViva(teachersF.toList, externalsF.toList))
     } yield
       val scheduledVivasResult = Algorithm.scheduleAllVivas(vivas, teachers, externals, agendaDuration)
       scheduledVivasResult
--- src/test/scala/pj/domain/schedule/properties/VivaProperties.scala
+++ src/test/scala/pj/domain/schedule/properties/VivaProperties.scala
@@ -6,7 +6,7 @@ import pj.domain.schedule.Preference
 import pj.domain.schedule.SimpleTypes.{Role, agendaDuration, availabilityEnd, availabilityStart, resourceId, resourceName, vivaStudent, vivaTitle}
 import pj.domain.schedule.Utils.stringToDuration
 import pj.domain.schedule.generators.AvailabilityGenerators.{generateAvailability, generateDuration, generatePreference}
-import pj.domain.schedule.generators.ResourceGenerators.{generateResourceId, generateResourceName, generateResources, generateThreeDigitNumber}
+import pj.domain.schedule.generators.ResourceGenerators.{generateResourceId, generateResourceName, generateThreeDigitNumber, generateUniqueResources}
 import pj.domain.schedule.generators.VivaGenerators.{generateInstersectedAvailabilities, generateRoles, generateScheduledVivas, generateStudent, generateTitle, generateViva}
 
 import java.time.{Duration, LocalDateTime, LocalTime}
@@ -18,7 +18,7 @@ object VivaProperties extends Properties("VivaProperties"):
   override def overrideParameters(p: Test.Parameters): Test.Parameters =
     p.withMinSuccessfulTests(500)
 
-  val dateFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd:HH:mm:ss")
+  val dateFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd:HH:mm")
   val timeFormatter = DateTimeFormatter.ofPattern("HH:mm:ss")
   val MIN_VIVAS = 1
   val MAX_VIVAS = 20
@@ -33,10 +33,7 @@ object VivaProperties extends Properties("VivaProperties"):
       sv._1.forall {
         case availability =>
           val isScheduledInAvailableInterval = resources.exists(a => (a.start.to.isBefore(availability.start.to) || a.start.to.isEqual(availability.start.to)) && (a.end.to.isAfter(availability.end.to) || a.end.to.isEqual(availability.end.to)))
-          if (!isScheduledInAvailableInterval) {
-            println("Availability: " + availability)
-            println("Resources: " + resources)
-          }
+
           isScheduledInAvailableInterval
       }
     })
@@ -50,11 +47,7 @@ object VivaProperties extends Properties("VivaProperties"):
 
         resources.combinations(2).forall { case Seq((resourceKeys1, availability1), (resourceKeys2, availability2)) =>
           val commonResources = resourceKeys1.intersect(resourceKeys2)
-          println("Resource Keys 1: " + resourceKeys1)
-          println("Resource Keys 2: " + resourceKeys2)
-          println("Availability 1: " + availability1)
-          println("Availability 2: " + availability2)
-          println("Common Resources: " + commonResources)
+
           commonResources.isEmpty || (availability1.end.to.isBefore(availability2.start.to) || availability1.end.to.isEqual(availability2.start.to) || availability1.start.to.isAfter(availability2.end.to) || availability1.start.to.isEqual(availability2.end.to))
         }
       case _ => false
@@ -66,48 +59,26 @@ object VivaProperties extends Properties("VivaProperties"):
       val scheduledVivas = sv._1
       scheduledVivas.forall { case vivaAvailability1@(viva1, availability1) =>
         val noOverlap = scheduledVivas.filterNot(_ == vivaAvailability1).forall { case (viva2, availability2) =>
-          println("Viva 1: " + viva1)
-          println("Availability 1: " + availability1)
-          println("Viva 2: " + viva2)
-          println("Availability 2: " + availability2)
+
           (availability1.end.to.isBefore(availability2.start.to) || availability1.end.to.isEqual(availability2.start.to) || availability1.start.to.isAfter(availability2.end.to) || availability1.start.to.isEqual(availability2.end.to))        }
-        if (!noOverlap) {
-        }
+
         noOverlap
       }
     })
   }
   
   
-  property("The generated resources should unique") = Prop.forAll(generateInstersectedAvailabilities) { scheduledVivas =>
+  property("The generated resources should be unique") = Prop.forAll(generateInstersectedAvailabilities) { scheduledVivas =>
     scheduledVivas match
-      case Right((vivas, teachers, externals, _)) =>
+      case Right((_, teachers, externals, _)) =>
         val resources = teachers ++ externals
         val resourceIds = resources.map(_.id.to)
-        println("Resource Ids: " + resourceIds)
+
         resourceIds.distinct.sizeIs == resourceIds.sizeIs
       case _ => false
   }
 
-
-  property("Check if the number of roles in the schedule viva is the same as the initial viva resources") = Prop.forAllNoShrink(generateScheduledVivas) { scheduledVivas =>
-    scheduledVivas match
-      case Right((vivas, teachers, externals, _)) =>
-        println("Teachers: " + teachers)
-        println("Externals: " + externals)
-        val resources = vivas.map { case (viva, availability) => (viva.roles.keys.toSeq, availability) }
-        resources.forall { case (resourceKeys, _) =>
-          println("Resource Keys: " + resourceKeys)
-          val allResources = teachers ++ externals
-          println("All Resources: " + allResources)
-          println("All Resources Size: " + allResources.size)
-          println("Resource Keys Size: " + resourceKeys.size)
-          resourceKeys.sizeIs == allResources.sizeIs
-        }
-      case _ => false
-  }
-
-
+  
   property("Check if one or more availabilities follow the minimum duration") = Prop.forAll(generateScheduledVivas) { scheduledVivas =>
     scheduledVivas match
       case Right((vivas, _, _, _)) =>
@@ -127,12 +98,11 @@ object VivaProperties extends Properties("VivaProperties"):
         requiredResources.flatMap(_._1).distinct.forall { resourceKey =>
           val resource = teachers.find(_.id.to.equals(resourceKey)).getOrElse(externals.find(_.id.to.equals(resourceKey)).getOrElse(???))
           val resourceAvailabilities = requiredResources.filter(_._1.contains(resourceKey)).map(_._2)
-          println("Resource: " + resource)
-          println("Resource Availabilities: " + resourceAvailabilities)
+
           val totalPreference = resourceAvailabilities.map(_.preference.d).sum
-          println("Total Preference: " + totalPreference)
+
           val scheduledVivasPreference = vivas.filter(_._1.roles.keys.toSeq.contains(resourceKey)).map { case (viva, availability) => availability.preference.d }.sum
-          println("Scheduled Vivas Preference: " + scheduledVivasPreference)
+
           totalPreference == scheduledVivasPreference
         }
       case _ => false
@@ -141,8 +111,9 @@ object VivaProperties extends Properties("VivaProperties"):
 
   property("Number of Roles must be at least 2 and at most 5") = Prop.forAll(for {
     duration <- generateDuration
-    resources <- Gen.listOfN(5, generateResources(duration))
-    roles <- generateRoles(resources)
+    teachers <- generateUniqueResources(5, duration, Set.empty, true)
+    externals <- generateUniqueResources(5, duration, teachers.map(_.id.to).toSet, false)
+    roles <- generateRoles(teachers, externals)
   } yield roles) { roles =>
     roles.sizeIs >= 2 && roles.sizeIs <= 5
   }
@@ -161,53 +132,45 @@ object VivaProperties extends Properties("VivaProperties"):
   property("Generate Viva Title should follow the pattern 'Title {3}'") = Prop.forAll(generateTitle) { title =>
     title.to.matches("Title \\d{3}")
   }
-  
-  
+
+
   property("Roles contain at least 1 President and 1 Advisor") = Prop.forAll(for {
     duration <- generateDuration
-    resources <- Gen.listOfN(5, generateResources(duration))
-    roles <- generateRoles(resources)
+    teachers <- generateUniqueResources(5, duration, Set.empty, true)
+    externals <- generateUniqueResources(5, duration, teachers.map(_.id.to).toSet, false)
+    roles <- generateRoles(teachers, externals)
     } yield roles) { roles =>
-    roles.values.toList.contains(Role.President) && roles.values.toList.contains(Role.Advisor)
+    roles.values.exists(_ == Role.President) && roles.values.exists(_ == Role.Advisor)
   }
-  
-  
+
+
   property("Generate ResourceId follows the correct pattern") = Prop.forAll(generateResourceId("T", Gen.oneOf(generateThreeDigitNumber).sample.getOrElse("0"))) { (resourceId: resourceId) =>
     //Check if resourceId follows the pattern "T\d{3}" or "E\d{3}"
-    println("Resource Id: " + resourceId)
+
     resourceId.to.matches("T\\d{3}") || resourceId.to.matches("E\\d{3}")
   }
   
   
   property("Generate ResourceName follows the correct pattern") = Prop.forAll(generateResourceName("Teacher ", Gen.oneOf(generateThreeDigitNumber).sample.getOrElse("0"))) { (resourceName: resourceName) =>
     //Check if resourceId follows the pattern "Teacher \d{3}" or "External \d{3}"
-    println("Resource Name: " + resourceName)
+
     resourceName.to.matches("Teacher \\d{3}") || resourceName.to.matches("External \\d{3}")
   }
   
   
   property("Generate Duration must be between 1 and 2 hours") = Prop.forAll(generateDuration) { (duration: agendaDuration) =>
     //println("Duration: " + duration)
-    //Agenda Duration must be min 1 hour and max 2 hours
     val durationTime = DateTimeFormatter.ofPattern("HH:mm:ss").format(LocalTime.parse(duration.to, timeFormatter))
     val duration2 = stringToDuration(durationTime).getOrElse(Duration.ZERO)
     duration2.toHours >= 1 && duration2.toHours <= 2
   }
-  
-  
-  property("Generate vivas") = Prop.forAll(Gen.chooseNum(2, 5)) { (n: Int) =>
-    val vivaListGen = Gen.listOfN(n, generateViva)
-    vivaListGen.sample match
-      case Some(vivaList) => vivaList.sizeIs == n
-      case None => false
-  }
-  
+    
   
   property("Generate Preference") = Prop.forAll(generatePreference) { preference =>
     preference.d >= 1 && preference.d <= 5
   }
   
-  
+
   property("Generate Availability") = Prop.forAll(generateDuration.flatMap(duration => generateAvailability(duration))) { availability =>
     availability.start.to.toString.nonEmpty && availability.end.to.toString.nonEmpty
   }
